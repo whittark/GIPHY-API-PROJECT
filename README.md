2447f3bdec1c3263b92760e4664fb9ec244182fa
Whitney Tarkington <whittark@gmail.com>
2019-09-04 00:08:49 -0700

    Adding HTML and assets

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# GIPHY-API-PROJECT
-Ajax API call to GIPHY; JS + JQuery to add GIF responses to HTML. Code to animate from still states and add/remove GIFs.
+# Explore animated GIFs from Studio GHIBLI films
+Select buttons to view beloved characters from such animated epics as Spirited Away, Ponyo, Howl's Moving Castle.
+
+Select a still image to watch the image animate!
+
+# Add your own favorites
+In Add a favorite, enter a keyword; click Select to add it as a button.
+Select the added button to view images.
+
+Select Remove to remove the added button.
+
+Enjoy!
+
+
---
+++ assets/javascript/app.js
@@ -0,0 +1,115 @@
+//Document load
+$(document).ready(function() {
+
+//Array of Studio Ghibli characters
+var gChar = ["Catbus", "Totoro", "Kaonashi", "Wizard Howl", "Ponyo", "Sosuke", "Chihiro Ogino","Calcifer","Yubaba", "Ashitaka", "Sophie Hatter","Radish Spirit", "Ghibli Cat Returns", "Granmamare",  "Kakashi no Kabu", "Dragon Haku", "Kawa no Kami", "Mononoke"];	
+
+//DISPLAY BUTTONS
+//Function that (1)iterates through the array, (2)creates a space
+//for buttons, (3)creates a button for each array item
+//(4)adds classes, (5)stores/adds button names (6)appends buttons
+function displayGifButtons() {
+	$("#gifButtonsView").empty();
+	for (var i = 0; i < gChar.length; i++) {
+		var gifButton = $("<button>");
+		gifButton.addClass("ghibli");
+		gifButton.addClass("btn btn-primary");
+		gifButton.attr("data-name", gChar[i]);
+		gifButton.text(gChar[i]);
+		$("#gifButtonsView").append(gifButton);
+	}
+}
+
+//ADD NEW BUTTON
+//Function with (1)listener for Submit button click, 
+//(2)takes in field input,
+//(3)pushes as new button if text submission is not empty
+function addNewButton() {
+	$("#addGif").on("click", function() {
+		var ghibli = $("#gCharInput").val().trim();
+		if (ghibli == ""){  //if blank on Submit
+			return false; 
+		}
+		gChar.push(ghibli);
+		displayGifButtons();
+		return false;
+	});
+}
+
+//REMOVE ADDED BUTTON
+//Function with (1)listner for Remove button selection,
+//(2) pop method to remove last added button from 
+//displayGifButtons()
+function removeLastButton() {
+	$("removeGif").on("click", function() {
+		gChar.pop(ghibli);
+		displayGifButtons();
+		return false;
+	});
+}
+
+//DISPLAY GIFS USING GIPHY API
+//(1)Grabs the data-name from selected button (2)URL
+//and AJAX query to get GIF (3)response handling
+function displayGifs() {
+	var ghibli = $(this).attr("data-name");
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + ghibli + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=6&rating=G";
+
+	$.ajax({
+		url: queryURL,
+		method: 'GET'
+	})
+
+	.done(function(response) {
+		$("#gifsView").empty();
+		var results = response.data;
+		if (results == "") {
+			alert("No GIF found"); //Message if no GIF
+		}
+		for (var i = 0; i<results.length; i++) {
+
+			var gifDiv = $("<div1>");
+
+			var gifRating = $("<p>").text("Rating " + results[i].rating);
+
+			gifDiv.append(gifRating); //Appends rating
+			
+			var gifImage = $("<img>"); //Creates image element
+
+			gifImage.attr("src", results[i] .images.fixed_height_small_still.url); //Grabs still, small image
+
+			gifImage.attr("data-still", results[i].images.fixed_height_small_still.url); //Adds still attr
+
+			gifImage.attr("data-animate", results[i].images.fixed_height_small.url); //Adds animate attr
+
+			gifImage.attr("data-state", "still"); //Defaults to still
+
+			gifImage.addClass("image");
+
+			gifDiv.append(gifImage); //Adds gif to gifDiv as still with animated stored
+
+			$("#gifsView").prepend(gifDiv);
+		}
+	});
+}
+
+//CALL FUNCTIONS
+displayGifButtons();
+addNewButton();
+removeLastButton();
+
+//LISTENERS FOR IMAGE CLICK: STILL & ANIMATE TOGGLE
+$(document).on("click", ".ghibli", displayGifs);
+$(document).on("click", ".image", function(){
+	var state = $(this).attr('data-state');
+	if (state == 'still') {
+		$(this).attr('src', $(this).data('animate'));
+		$(this).attr('data-state', 'animate');
+	}else {
+		$(this).attr('src', $(this),data('still'));
+		$(this).attr('data-state', 'still');
+	}
+});
+
+//Script End
+});
